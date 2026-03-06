3c6428c196704ff9876dd0f393a21f1d71e67c5e
Mysa-vlad <mysiienko.vlad14062007@gmail.com>
2026-03-06 09:27:14 +0000

    Deploying to gh-pages from @ Mysa-vlad/goit-js-hw-09@37e49c9fc12c0c1abd94aedd0c27aba6a8bca42c 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BPGxNxDt.js";const e=document.querySelector(".feedback-form");e.addEventListener("input",()=>{});
+//# sourceMappingURL=2-form.js.map
