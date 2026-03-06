bad50c2dda7a12a23cfbc95b124fa53e0d10126c
Musiienko <mysiienko.vlad14062007@gmail.com>
2026-03-06 11:21:44 +0200

    form css

---
+++ src/js/2-form.js
@@ -0,0 +1,10 @@
+import { Input } from "postcss"
+
+const formData = {
+    email: "", 
+    message: ""
+}
+const feedbackForm = document.querySelector(".feedback-form")
+feedbackForm.addEventListener('input', ()=>{
+    
+})
